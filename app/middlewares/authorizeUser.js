3fcc1456eceb89f2626a263888616a59de5880fb
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-07-02 12:42:40 +0530

    authorizeUser added

---
+++ app/middlewares/authorizeUser.js
@@ -0,0 +1,11 @@
+const authorizeUser = (permissions) =>{
+    return(req, res, next)=>{
+        if(permissions.includes(req.user.role)){
+            next()
+        } else{
+            res.status(403).json({errors: "Unauthorized user"})
+        }
+    }
+}
+
+module.exports = authorizeUser
