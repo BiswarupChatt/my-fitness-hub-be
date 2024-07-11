8beeb2994dc56203e75ba0c9d7b10ecd8c564b43
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-07-11 22:52:49 +0530

    postCtrl update v3

--- app/controllers/progress-ctrl.js
+++ app/controllers/progress-ctrl.js
@@ -59,13 +59,14 @@ progressCtrl.update = async (req, res) => {
             thigh: req.body.thigh,
             bicep: req.body.bicep
         }
-
         const findProgress = await Progress.findById(req.params._id)
-
+        if (!findProgress) {
+            return res.status(404).json({ errors: "Progress record not found" })
+        }
         if (findProgress.client._id.toString() !== req.user.id.toString()) {
             return res.status(404).json({ errors: "You are not authorized to update progress" })
         }
-
+        
         const updatedProgress = await Progress.findByIdAndUpdate(req.params._id, progressData, { new: true })
         if (!updatedProgress) {
             return res.status(404).json({ errors: "Progress record not found" })
