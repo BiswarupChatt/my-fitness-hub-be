037eeb74557ef3779399ded794e01a7e08ad348e
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-07-19 10:41:13 +0530

    subscription model

---
+++ app/models/subscription-model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const subscriptionSchema = new Schema({
+    coach: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    paymentId: {
+        type: String,
+        required: true
+    },
+    orderId: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true,
+        default: 'INR'
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'failed', 'success'],
+        default: 'pending'
+    }
+}, { timestamps: true })
+
+const Subscription = model('Subscription', subscriptionSchema)
+
+module.exports = Subscription
